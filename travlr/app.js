cfd0e077f77010834f60d39e98dc37c24a995c24
Jackson Foster <33702585+jfoster063001@users.noreply.github.com>
2023-09-10 12:43:39 -0500

    fixed errors

--- travlr/app.js
+++ travlr/app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const hbs = require('hbs');
+
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
@@ -12,11 +14,12 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
+app.set('view engine', 'hbs');
 
 // register handlebars partials (https://www.npmjs.com/package/hbs)
 hbs.registerPartials(path.join(__dirname, 'app_server','views/partials'));
 
-app.set('view engine', 'hbs');
+
 
 app.use(logger('dev'));
 app.use(express.json());
