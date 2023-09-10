0fd6f0bf19d1546bdbc4a1dfa62ca1be7bf780ba
Jackson Foster <33702585+jfoster063001@users.noreply.github.com>
2023-09-10 12:23:22 -0500

    fixed one error

--- travlr/app.js
+++ travlr/app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
-var travelRouter = require('./app_server/routes/users');
+var travelRouter = require('./app_server/routes/travel');
 
 var app = express();
 
