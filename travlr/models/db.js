f43b11fd4090442f2b4e77e355189afc42599571
Jackson Foster <33702585+jfoster063001@users.noreply.github.com>
2023-09-24 12:36:10 -0500

    test

--- travlr/models/db.js
+++ travlr/models/db.js
@@ -8,7 +8,8 @@ mongoose.set('useUnifiedTopology', true);
 const connect = () => {
     setTimeout(() => mongoose.connect(dbURI,{
         useNewUrlParser: true,
-        useCreateIndex: true
+        useCreateIndex: true,
+        useUnifiedTopology: true
     }), 1000);
 }
 
