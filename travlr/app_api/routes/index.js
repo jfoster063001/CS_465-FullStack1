8ba501ef8541c34d831e401426d4df1708757ec1
Jackson Foster <33702585+jfoster063001@users.noreply.github.com>
2023-09-28 20:42:26 -0500

    test

--- travlr/app_api/routes/index.js
+++ travlr/app_api/routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const ctrlMain = require('../controllers/main');
+const ctrlMain = require('../controllers/trips');
 
 /* GET home page. */
-router.get('/', ctrlMain.index);
+router
+    .route('/trips')
+    .get(tripsController.tripsList);
 
 module.exports = router;
