91f7372c82f54e0d359ee2493d9e567fe0b274f7
Jackson Foster <33702585+jfoster063001@users.noreply.github.com>
2023-09-28 20:10:21 -0500

    new changes

---
+++ travlr/app_api/controllers/trips.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+const Model = mongoose.model('trips');
+
+//get list of trips
+const tripsList = async (req, res) => {
+    Model
+        .find({})//empty filter
+        .exec((err, trips) => {
+            if (!trips){
+                return res
+                    .status(404)
+                    .json({"message":"Trips not found"});
+            } else if (err) {
+                return res
+                    .status(404)
+                    .json(err);
+            } else{
+                return res
+                    .status(200)
+                    .json(trips);
+            }
+        });
+};
+
+//trips by code
+const tripsFindByCode = async (req, res) => {
+    Model
+        .find({'code': req.params.tripcode })
+        .exec((err, trip) => {
+            if (!trip){
+                return res
+                    .status(404)
+                    .json({"message":"Trips not found"});
+            } else if (err) {
+                return res
+                    .status(404)
+                    .json(err);
+            } else{
+                return res
+                    .status(200)
+                    .json(trips);
+            }
+        });
+};
+
+module.exports = {
+    tripsList,
+    tripsFindByCode
+};
