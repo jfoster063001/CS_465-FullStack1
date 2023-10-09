d184ad55bd58c3b5f32bb31b5a6bde989ac9cddd
Jackson Foster <33702585+jfoster063001@users.noreply.github.com>
2023-10-08 22:51:18 -0500

    got it fixed

--- travlr/app_server/controllers/travel.js
+++ travlr/app_server/controllers/travel.js
@@ -1,4 +1,5 @@
-const axios = require('axios');
+const axios = require('request');
+const { request } = require('../../app');
 const apiOptions = {
  server: 'http://localhost:3000'
 };
@@ -12,38 +13,42 @@ var trips = JSON.parse(fs.readFileSync('./data/trips.json','utf8'));
 const renderTravelList = (req, res, responseBody) => {
     let message = null;
     let pageTitle = process.env.npm_package_description + ' - Travel';
-    if(!(responseBody instanceof Array)){
+    if (!(responseBody instanceof Array)) {
         message = 'API lookup error';
         responseBody = [];
     } else {
-        if (!responseBody.length){
+        if (!responseBody.length) {
             message = 'No trips exist in our database!';
         }
     }
     res.render('travel',
-    {
-        title: pageTitle,
-        trips: responseBody,
-        message
-    }
+        {
+            title: pageTitle,
+            trips: responseBody,
+            message
+        }
     );
 }
 
-const travelList = async (req, res) => {
-   const path = '/api/trips';
-   const requestOptions ={
-    url: `${apiOptions.server}${path}`,
-    method: 'GET',
-    json: {},
-   };
-   console.info('>>travelController.traveList calling' + requestOptions.url);
-   console.log('line 41 in controller travel.js');
-   try {
-    const response = await axios(requestOptions);
-    renderTravelList(req, res, response.data);
-  } catch (error) {
-    console.error(error);
-    renderTravelList(req, res, []);
-  }
+const travelList = (req, res) => {
+    const path = '/api/trips';
+    const requestOptions = {
+        url: `${apiOptions.server}${path}`,
+        method: 'GET',
+        json: {},
+    };
+    console.info('>>travelController.travelList calling ' + requestOptions.url);
+    request(
+        requestOptions,
+        (err, { stausCode }, body) => {
+            if (err) {
+                console.error(err);
+            }
+            renderTravelList(req, res, body);
+        }
+    );
 };
-   
+
+module.exports = {
+    travelList
+};
