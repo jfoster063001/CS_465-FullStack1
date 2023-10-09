7ff9c0bd5091b0a3c7a02e676a1b8a5d22149cc7
Jackson Foster <33702585+jfoster063001@users.noreply.github.com>
2023-10-08 23:00:54 -0500

    on step 45

--- travlr/app_api/controllers/trips.js
+++ travlr/app_api/controllers/trips.js
@@ -44,7 +44,7 @@ const tripsFindByCode = async (req, res) => {
 };
 
 const tripsAddTrip = async (req, res) => {
-    model
+    Model
     .create({
         code: req.body.code,
         name: req.body.name,
@@ -69,8 +69,48 @@ const tripsAddTrip = async (req, res) => {
     });
 }
 
+const tripsUpdateTrip = async (req, res) => {
+    console.log(req.body);
+    Model
+        .findOneAndUpdate({ 'code': req.params.tripCode }, {
+            code: req.body.code,
+            name: req.body.name,
+            length: req.body.length,
+            start: req.body.start,
+            resort: req.body.resort,
+            perPerson: req.body.perPerson,
+            image: req.body.image,
+            description: req.body.description
+        }, { new: true })
+        .then(trip => {
+            if (!trip) {
+                return res
+                    .status(404)
+                    .send({
+                        message: "Trip not found with code "
+                            + req.params.tripCode
+                    });
+            }
+            res.send(trip);
+        }).catch(err => {
+            if (err.kind === 'ObjectId') {
+                return res
+                    .status(404)
+                    .send({
+                        message: "Trip not found with code "
+                            + req.params.tripCode
+                    });
+            }
+            return res
+                .status(500) // server error
+                .json(err);
+        });
+}
+   
+
 module.exports = {
     tripsList,
     tripsFindByCode,
-    tripsAddTrip
+    tripsAddTrip,
+    tripsUpdateTrip
 };
