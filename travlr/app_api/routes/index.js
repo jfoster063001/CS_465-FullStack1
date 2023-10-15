96517b547382c720c5562189aae94c8076697e0f
Jackson Foster <33702585+jfoster063001@users.noreply.github.com>
2023-10-15 13:38:57 -0500

    added new pages

--- travlr/app_api/routes/index.js
+++ travlr/app_api/routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const {expressjwt: jwt} = require('express-jwt');
 
 const auth = jwt({
+    
     secret: process.env.JWT_SECRET,
     userProperty: "payload",
+    algorithms: ["HS256"],
 });
 
 const authController = require('../controllers/authentication');
