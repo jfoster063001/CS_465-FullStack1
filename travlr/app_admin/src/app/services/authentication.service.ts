3e59a17b28948b530ddba83ffdb9e396d549e2d6
Jackson Foster <33702585+jfoster063001@users.noreply.github.com>
2023-10-15 20:28:13 -0500

    more fixes

--- travlr/app_admin/src/app/services/authentication.service.ts
+++ travlr/app_admin/src/app/services/authentication.service.ts
@@ -6,30 +6,37 @@ import { TripDataService } from '../services/trip-data.service';
 @Injectable({
   providedIn: 'root'
 })
+
 export class AuthenticationService {
   constructor(
     @Inject(BROWSER_STORAGE) private storage: Storage,
     private tripDataService: TripDataService
   ) { }
+
   public getToken(): string {
     return this.storage.getItem('travlr-token');
   }
+
   public saveToken(token: string): void {
     this.storage.setItem('travlr-token', token);
   }
+
   public login(user: User): Promise<any> {
     return this.tripDataService.login(user)
       .then((authResp: AuthResponse) =>
         this.saveToken(authResp.token));
   }
+
   public register(user: User): Promise<any> {
     return this.tripDataService.register(user)
       .then((authResp: AuthResponse) =>
         this.saveToken(authResp.token));
   }
+
   public logout(): void {
     this.storage.removeItem('travlr-token');
   }
+
   public isLoggedIn(): boolean {
     const token: string = this.getToken();
     if (token) {
@@ -39,6 +46,7 @@ export class AuthenticationService {
       return false;
     }
   }
+  
   public getCurrentUser(): User {
     if (this.isLoggedIn()) {
       const token: string = this.getToken();
